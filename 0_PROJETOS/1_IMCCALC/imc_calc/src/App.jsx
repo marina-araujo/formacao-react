ef439fcd43f4f017e9d86b97631c296b33d7482d
Marina Barbosa Araujo <marina.ba@gmail.com>
2026-03-17 12:01:36 -0300

    Projeto 15 - IMC Calc no React - Iniciando projeto

---
+++ 0_PROJETOS/1_IMCCALC/imc_calc/src/App.jsx
@@ -0,0 +1,15 @@
+import CalcImc from './components/CalcImc';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='container'>
+      
+      <CalcImc />
+    </div>
+  )
+}
+
+export default App
