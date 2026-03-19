5c2c4945ca5bfb9e485ca7ae1d76eca9e95695d4
Marina Barbosa Araujo <marina.ba@gmail.com>
2026-03-19 12:03:00 -0300

    seção 41 - projeto imc react - finalizando

--- 0_PROJETOS/1_IMCCALC/imc_calc/src/components/CalcImc.jsx
+++ 0_PROJETOS/1_IMCCALC/imc_calc/src/components/CalcImc.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-import './CalcImc.css'
+import { useState } from 'react'
+
 import Button from './Button'
 
-const CalcImc = () => {
+import './CalcImc.css'
+
+const CalcImc = ({ calcImc }) => {
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+
+    const clearForm = (e) => {
+        e.preventDefault();
+        setWeight("");
+        setHeight("");
+    }
+
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, "")
+    }
+
+    const handleHeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value);
+        setHeight(updatedValue);
+    }
+
+    const handleWeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value);
+        setWeight(updatedValue);
+    }
+
   return (
       <div id='calc-container'>
           <h2>Calculadora de IMC</h2>
@@ -11,25 +36,29 @@ const CalcImc = () => {
                 <div className='form-control'>
                   <label htmlFor="height">Altura:</label>
                   <input
-                      type="text"
-                      name='height'
-                      id='height'
-                      placeholder='Exemplo 1,75'
+                          type="text"
+                          name='height'
+                          id='height'
+                          placeholder='Exemplo 1,75'
+                          onChange={(e) => handleHeightChange(e)}
+                          value={height}
                   />
                   </div>
                 <div className='form-control'>
                   <label htmlFor="weight">Peso:</label>
-                  <input
-                      type="text"
-                      name='weight'
-                      id='weight'
-                      placeholder='Exemplo 70,5'
+                      <input
+                          type="text"
+                          name='weight'
+                          id='weight'
+                          placeholder='Exemplo 70,5'
+                          onChange={(e) => handleWeightChange(e)}
+                          value={weight}
                   />
                 </div>
               </div>
               <div className="action-control">
-                  <Button id="calc-btn" text="Calcular" />
-                  <Button id="clear-btn" text="Limpar" />
+                  <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)} />
+                  <Button id="clear-btn" text="Limpar" action={clearForm} />
               </div>
           </form>
     </div>
